c226d09d5edaa419d9e948ce0cb870f23894bcb2
Edeh Emeka <emekus29kb@gmail.com>
2022-06-14 15:43:59 +0200

    Add files via upload

---
+++ BP_Test.js
@@ -0,0 +1,37 @@
+
+//BP Analysis Function
+function BP() {
+    let topValue = document.querySelector('#top').value;
+    let bottomValue = document.querySelector('#bottom').value;
+    
+    if (topValue && bottomValue != ""){
+        if (topValue < 120 && bottomValue < 80){
+            //alert("Blood Pressure is NORMAL")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "lightgreen";
+            result.innerHTML = "Blood Pressure is NORMAL"
+        } else if ((topValue == 120 || topValue <= 129) && (bottomValue < 80)) {
+            //alert("Blood Pressure is ELEVATED")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "orange";
+            result.innerHTML = "Blood Pressure is ELEVATED"
+        } else if ((topValue == 130 || topValue <= 139) || ((bottomValue == 80) || (bottomValue <= 89))) {
+            //alert("Blood Pressure is High (Satage 1 Hypertension)")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "darkorange";
+            result.innerHTML = "Blood Pressure is High (Stage 1 Hypertension)"
+        } else if (((topValue > 140) && (topValue <= 140)) || ((bottomValue > 90)) && (bottomValue <=120) ) {
+            //alert("Blood Pressure is High (Satage 2 Hypertension)")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "red";
+            result.innerHTML = "Blood Pressure is High (Stage 2 Hypertension)"
+        } else if ((topValue > 180) && (bottomValue > 120)) {
+            //alert("Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "darkred";
+            result.innerHTML = "Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)"
+        }
+    }else{
+        alert("Can't generate result from Nothing; Please input values")
+    }
+}
