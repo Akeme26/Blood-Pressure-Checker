3d485302afd32624d98664a431c5e511a6d40a12
Edeh Emeka <emekus29kb@gmail.com>
2022-07-14 11:02:37 +0200

    Form return false
    modification of BP app features to prevent form submission and save user input in a list of dictionaries

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, redirect, url_for
 
 app = Flask("BP App")
 
@@ -12,7 +12,13 @@ def root():
 
 @app.route('/result', methods = ['POST'])
 def result():
-    pass
+    if request.method == 'POST':
+        name = request.form['Name']
+        email = request.form['Email']
+        
+        patient_details.append({"name":name, "email":email})
+
+        return patient_details[-1]
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/BP_Test.js
+++ static/BP_Test.js
@@ -1,9 +1,52 @@
-let details = ()=>{  
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("updatemessage").innerHTML = xhttp.responseText;        }
-    };
-    xhttp.open("GET", "/result", true);
-    xhttp.send();
+//Toggle Submit button color 
+let resultButton = document.getElementById("Gen_result");
+resultButton.addEventListener('click', function(){
+    if (resultButton.style.backgroundColor == 'red') {
+        resultButton.style.backgroundColor = 'green'}
+    else{
+        resultButton.style.backgroundColor = 'red';
+    }
+})
+
+
+//BP Analysis Function
+function BP() {
+    let topValue = document.querySelector('#top').value;
+    let bottomValue = document.querySelector('#bottom').value;
+    
+    if (topValue && bottomValue != ""){
+        if (topValue < 120 && bottomValue < 80){
+            //alert("Blood Pressure is NORMAL")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "lightgreen";
+            result.innerHTML = "Blood Pressure is NORMAL"
+        } else if ((topValue >= 120 && topValue <= 129) && (bottomValue < 80)) {
+            //alert("Blood Pressure is ELEVATED")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "orange";
+            result.innerHTML = "Blood Pressure is ELEVATED"
+        } else if ((topValue >= 130 && topValue <= 139) || (bottomValue >= 80 && bottomValue <= 89)) {
+            //alert("Blood Pressure is High (Stage 1 Hypertension)")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "darkorange";
+            result.innerHTML = "Blood Pressure is High (Stage 1 Hypertension)"
+        } else if ((topValue >= 140 && topValue <= 180) || (bottomValue >= 90 && bottomValue <=120) ) {
+            //alert("Blood Pressure is High (Stage 2 Hypertension)")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "red";
+            result.innerHTML = "Blood Pressure is High (Stage 2 Hypertension)"
+        } else if ((topValue > 180) && (bottomValue > 120)) {
+            //alert("Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "darkred";
+            result.innerHTML = "Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)"
+        } else if ((topValue > 180) || (bottomValue > 120)) {
+            //alert("Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)")
+            result = document.getElementById("result1");
+            result.style.backgroundColor = "darkred";
+            result.innerHTML = "Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)"
+        }
+    }else{
+        alert("Can't generate result from Nothing; Please input values")
+    }
 }
