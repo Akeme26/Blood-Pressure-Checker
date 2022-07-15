6b4b9c4f42e1b63a7de8a5a1c46135ffb549cd7c
Edeh Emeka <emekus29kb@gmail.com>
2022-07-15 21:17:30 +0200

    display edits
    added border to result text area

--- server.py
+++ server.py
@@ -15,10 +15,16 @@ def result():
     if request.method == 'POST':
         name = request.form['Name']
         email = request.form['Email']
+        systolic = request.form['topValue']
+        diastolic = request.form['bottomValue']
         
-        patient_details.append({"name":name, "email":email})
+        patient_details.append({"name":name, "email":email, "topValue":systolic, "bottomValue":diastolic})
 
         return patient_details[-1]
+
+@app.route('/email')
+def test():
+    return patient_details[0]
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/BP_Test.js
+++ static/BP_Test.js
@@ -20,33 +20,45 @@ function BP() {
             result = document.getElementById("result1");
             result.style.backgroundColor = "lightgreen";
             result.innerHTML = "Blood Pressure is NORMAL"
-        } else if ((topValue >= 120 && topValue <= 129) && (bottomValue < 80)) {
+            Glow();
+        } else if ((topValue >= 120 && topValue <= 129) && (bottomValue < 80)) {9
             //alert("Blood Pressure is ELEVATED")
             result = document.getElementById("result1");
             result.style.backgroundColor = "orange";
-            result.innerHTML = "Blood Pressure is ELEVATED"
+            result.innerHTML = "Blood Pressure is ELEVATED";
+            Glow();
         } else if ((topValue >= 130 && topValue <= 139) || (bottomValue >= 80 && bottomValue <= 89)) {
             //alert("Blood Pressure is High (Stage 1 Hypertension)")
             result = document.getElementById("result1");
             result.style.backgroundColor = "darkorange";
-            result.innerHTML = "Blood Pressure is High (Stage 1 Hypertension)"
+            result.innerHTML = "Blood Pressure is High (Stage 1 Hypertension)";
+            Glow();
         } else if ((topValue >= 140 && topValue <= 180) || (bottomValue >= 90 && bottomValue <=120) ) {
             //alert("Blood Pressure is High (Stage 2 Hypertension)")
             result = document.getElementById("result1");
             result.style.backgroundColor = "red";
-            result.innerHTML = "Blood Pressure is High (Stage 2 Hypertension)"
+            result.innerHTML = "Blood Pressure is High (Stage 2 Hypertension)";
+            Glow();
         } else if ((topValue > 180) && (bottomValue > 120)) {
             //alert("Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)")
             result = document.getElementById("result1");
             result.style.backgroundColor = "darkred";
-            result.innerHTML = "Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)"
+            result.innerHTML = "Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)";
+            Glow();
         } else if ((topValue > 180) || (bottomValue > 120)) {
             //alert("Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)")
             result = document.getElementById("result1");
             result.style.backgroundColor = "darkred";
             result.innerHTML = "Patient is in Hypertensive Crisis (Seek Medical Attention ASAP)"
+            Glow();
         }
     }else{
         alert("Can't generate result from Nothing; Please input values")
     }
+}
+
+
+//Result Highlighter Function
+let Glow = () => {
+    const border = document.getElementById('result1').style.border = 'yellow';
 }
